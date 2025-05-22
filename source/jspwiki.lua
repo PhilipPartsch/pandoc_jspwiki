8ea2c8127300448387d656d6acfafde3cf5ee53f
PhilipPartsch <philip.partsch@googlemail.com>
2025-05-23 00:06:06 +0200

    Update output file names in test_pandoc workflow and enhance JSPWiki Lua reader for improved parsing

--- source/jspwiki.lua
+++ source/jspwiki.lua
@@ -1,107 +1,190 @@
-local lpeg = require("lpeg")
-local P, R, S, V, C, Ct, Cmt, Cg, Cb = lpeg.P, lpeg.R, lpeg.S, lpeg.V, lpeg.C, lpeg.Ct, lpeg.Cmt, lpeg.Cg, lpeg.Cb
+-- A sample custom reader for Creole 1.0 (common wiki markup)
+-- http://www.wikicreole.org/wiki/CheatSheet
 
-local pandoc = require("pandoc")
-local List = require("pandoc.List")
+-- For better performance we put these functions in local variables:
+local P, S, R, Cf, Cc, Ct, V, Cs, Cg, Cb, B, C, Cmt =
+  lpeg.P, lpeg.S, lpeg.R, lpeg.Cf, lpeg.Cc, lpeg.Ct, lpeg.V,
+  lpeg.Cs, lpeg.Cg, lpeg.Cb, lpeg.B, lpeg.C, lpeg.Cmt
 
--- Utility patterns
-local newline = P("\r")^-1 * P("\n")
-local space = S(" \t")^0
-local non_newline = (1 - newline)
-local blankline = space * newline
+local whitespacechar = S(" \t\r\n")
+local specialchar = S("/*~[]\\{}|")
+local wordchar = (1 - (whitespacechar + specialchar))
+local spacechar = S(" \t")
+local newline = P"\r"^-1 * P"\n"
+local blankline = spacechar^0 * newline
+local endline = newline * #-blankline
+local endequals = spacechar^0 * P"="^0 * spacechar^0 * newline
+local cellsep = spacechar^0 * P"|"
 
--- Inline patterns
-local function make_inline_parser()
-  local any = P(1)
-  local str = (1 - S("'\n_"))^1
-
-  local bold = P("__") * C((1 - P("__"))^1) * P("__") / function(s)
-    return pandoc.Strong{pandoc.Str(s)}
-  end
-
-  local italic = P("''") * C((1 - P("''"))^1) * P("''") / function(s)
-    return pandoc.Emph{pandoc.Str(s)}
-  end
-
-  local word = C((1 - S(" \t\n"))^1) / function(s)
-    return pandoc.Str(s)
-  end
-
-  local space = P(" ") / function() return pandoc.Space() end
-
-  local inline = Ct((bold + italic + word + space)^1)
-  return inline
+local function trim(s)
+   return (s:gsub("^%s*(.-)%s*$", "%1"))
 end
 
-local inline = make_inline_parser()
-
--- Block parsing
-
-local function parse_blocks(text)
-  local blocks = List{}
-  local lines = {}
-
-  for line in text:gmatch("([^\r\n]*)\r?\n?") do
-    table.insert(lines, line)
+local function ListItem(lev, ch)
+  local start
+  if ch == nil then
+    start = S"*#"
+  else
+    start = P(ch)
   end
-
-  local i = 1
-  while i <= #lines do
-    local line = lines[i]
-
-    -- Headings
-    if line:match("^!!!") then
-      local content = line:match("^!!!%s*(.+)")
-      blocks:insert(pandoc.Header(1, inline:match(content)))
-    elseif line:match("^!!") then
-      local content = line:match("^!!%s*(.+)")
-      blocks:insert(pandoc.Header(2, inline:match(content)))
-    elseif line:match("^!") then
-      local content = line:match("^!%s*(.+)")
-      blocks:insert(pandoc.Header(3, inline:match(content)))
-
-    -- Unordered List
-    elseif line:match("^%*") then
-      local items = {}
-      while i <= #lines and lines[i]:match("^%*") do
-        local content = lines[i]:match("^%*+%s*(.+)")
-        table.insert(items, { pandoc.Plain(inline:match(content)) })
-        i = i + 1
-      end
-      blocks:insert(pandoc.BulletList(items))
-      goto continue
-
-    -- Ordered List
-    elseif line:match("^#") then
-      local items = {}
-      while i <= #lines and lines[i]:match("^#") do
-        local content = lines[i]:match("^#+%s*(.+)")
-        table.insert(items, { pandoc.Plain(inline:match(content)) })
-        i = i + 1
-      end
-      blocks:insert(pandoc.OrderedList(items))
-      goto continue
-
-    -- Paragraph
-    elseif line:match("%S") then
-      local para = line
-      -- Merge following lines until blank
-      while i + 1 <= #lines and lines[i + 1]:match("%S") do
-        i = i + 1
-        para = para .. " " .. lines[i]
-      end
-      blocks:insert(pandoc.Para(inline:match(para)))
+  local subitem = function(c)
+    if lev < 6 then
+      return ListItem(lev + 1, c)
+    else
+      return (1 - 1) -- fails
     end
-
-    ::continue::
-    i = i + 1
   end
-
-  return blocks
+  local parser = spacechar^0
+               * start^lev
+               * #(- start)
+               * spacechar^0
+               * Ct((V"Inline" - (newline * spacechar^0 * S"*#"))^0)
+               * newline
+               * (Ct(subitem("*")^1) / pandoc.BulletList
+                  +
+                  Ct(subitem("#")^1) / pandoc.OrderedList
+                  +
+                  Cc(nil))
+               / function (ils, sublist)
+                   return { pandoc.Plain(ils), sublist }
+                 end
+  return parser
 end
 
-function Reader(input, reader_opts)
-  local text = input
-  local blocks = parse_blocks(text)
-  return pandoc.Pandoc(blocks)
+-- Grammar
+G = P{ "Doc",
+  Doc = Ct(V"Block"^0)
+      / pandoc.Pandoc ;
+  Block = blankline^0
+        * ( V"Header"
+          + V"HorizontalRule"
+          + V"CodeBlock"
+          + V"List"
+          + V"Table"
+          + V"Para") ;
+  Para = Ct(V"Inline"^1)
+       * newline
+       / pandoc.Para ;
+  HorizontalRule = spacechar^0
+                 * P"----"
+                 * spacechar^0
+                 * newline
+                 / pandoc.HorizontalRule;
+  Header = (P("=")^1 / string.len)
+         * spacechar^1
+         * Ct((V"Inline" - endequals)^1)
+         * endequals
+         / pandoc.Header;
+  CodeBlock = P"{{{"
+            * blankline
+            * C((1 - (newline * P"}}}"))^0)
+            * newline
+            * P"}}}"
+            / pandoc.CodeBlock;
+  Placeholder = P"<<<"
+              * C(P(1) - P">>>")^0
+              * P">>>"
+              / function() return pandoc.Div({}) end;
+  List = V"BulletList"
+       + V"OrderedList" ;
+  BulletList = Ct(ListItem(1,'*')^1)
+             / pandoc.BulletList ;
+  OrderedList = Ct(ListItem(1,'#')^1)
+             / pandoc.OrderedList ;
+  Table = (V"TableHeader" + Cc{})
+        * Ct(V"TableRow"^1)
+        / function(headrow, bodyrows)
+            local numcolumns = #(bodyrows[1])
+            local aligns = {}
+            local widths = {}
+            for i = 1,numcolumns do
+              aligns[i] = pandoc.AlignDefault
+              widths[i] = 0
+            end
+            return pandoc.utils.from_simple_table(
+              pandoc.SimpleTable({}, aligns, widths, headrow, bodyrows))
+          end ;
+  TableHeader = Ct(V"HeaderCell"^1)
+              * cellsep^-1
+              * spacechar^0
+              * newline ;
+  TableRow   = Ct(V"BodyCell"^1)
+             * cellsep^-1
+             * spacechar^0
+             * newline ;
+  HeaderCell = cellsep
+             * P"="
+             * spacechar^0
+             * Ct((V"Inline" - (newline + cellsep))^0)
+             / function(ils) return { pandoc.Plain(ils) } end ;
+  BodyCell   = cellsep
+             * spacechar^0
+             * Ct((V"Inline" - (newline + cellsep))^0)
+             / function(ils) return { pandoc.Plain(ils) } end ;
+  Inline = V"Emph"
+         + V"Strong"
+         + V"LineBreak"
+         + V"Link"
+         + V"URL"
+         + V"Image"
+         + V"Str"
+         + V"Space"
+         + V"SoftBreak"
+         + V"Escaped"
+         + V"Placeholder"
+         + V"Code"
+         + V"Special" ;
+  Str = wordchar^1
+      / pandoc.Str;
+  Escaped = P"~"
+          * C(P(1))
+          / pandoc.Str ;
+  Special = specialchar
+          / pandoc.Str;
+  Space = spacechar^1
+        / pandoc.Space ;
+  SoftBreak = endline
+            * # -(V"HorizontalRule" + V"CodeBlock")
+            / pandoc.SoftBreak ;
+  LineBreak = P"\\\\"
+            / pandoc.LineBreak ;
+  Code = P"{{{"
+       * C((1 - P"}}}")^0)
+       * P"}}}"
+       / trim / pandoc.Code ;
+  Link = P"[["
+       * C((1 - (P"]]" + P"|"))^0)
+       * (P"|" * Ct((V"Inline" - P"]]")^1))^-1 * P"]]"
+       / function(url, desc)
+           local txt = desc or {pandoc.Str(url)}
+           return pandoc.Link(txt, url)
+         end ;
+  Image = P"{{"
+        * #-P"{"
+        * C((1 - (S"}"))^0)
+        * (P"|" * Ct((V"Inline" - P"}}")^1))^-1
+        * P"}}"
+        / function(url, desc)
+            local txt = desc or ""
+            return pandoc.Image(txt, url)
+          end ;
+  URL = P"http"
+      * P"s"^-1
+      * P":"
+      * (1 - (whitespacechar + (S",.?!:;\"'" * #whitespacechar)))^1
+      / function(url)
+          return pandoc.Link(pandoc.Str(url), url)
+        end ;
+  Emph = P"''"
+       * Ct((V"Inline" - P"''")^1)
+       * P"''"
+       / pandoc.Emph ;
+  Strong = P"__"
+         * Ct((V"Inline" -P"__")^1)
+         * P"__"
+         / pandoc.Strong ;
+}
+
+function Reader(input, reader_options)
+  return lpeg.match(G, tostring(input))
 end
